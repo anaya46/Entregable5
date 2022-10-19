440c6a9121c4c19c156417ce1f7757d010f3e600
anaya46 <sandraanaya46@hotmail.com>
2022-10-19 18:00:03 -0500

    repare pagination

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -6,60 +6,75 @@ import CharacterItem from './CharacterItem';
 import { useNavigate } from 'react-router-dom';
 
 const Pokedex = () => {
-//para acceder a lo que hay en la store:
-//el objeto que hay en el store se llama state,
-//accedo a la propiedad USER del objeto STATE.    
-    const user = useSelector(state =>state.user)
+    //para acceder a lo que hay en la store:
+    //el objeto que hay en el store se llama state,
+    //accedo a la propiedad USER del objeto STATE.    
+    const user = useSelector(state => state.user)
 
     const [characters, setCharacteres] = useState([])
     const [characterSearch, setcharacterSearch] = useState("");
-    const [types, setTypes] = useState ([])
+    const [types, setTypes] = useState([])
     const navigate = useNavigate();
 
-    useEffect(()=> {
+    useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154`)
-        .then(res => setCharacteres(res.data.results))
+            .then(res => setCharacteres(res.data.results))
 
         axios.get(`https://pokeapi.co/api/v2/type/`)
-        .then(res => setTypes(res.data.results))
+            .then(res => setTypes(res.data.results))
     }, []);
-  
+
 
     const search = e => {
         e.preventDefault();
         navigate(`/pokedex/${characterSearch}`)
 
     }
-    const filterType = e =>{
-            axios.get(e.target.value)
+    const filterType = e => {
+        axios.get(e.target.value)
             .then(res => setCharacteres(res.data.pokemon))
-            
+
     }
-    const [page,setPage] = useState(1)
+    const [page, setPage] = useState(1)
     const lastIndex = page * 12
     const firstIndex = lastIndex - 12
     const lastPage = Math.ceil(characters.length / 12)
-    const pokePaginated = characters.slice(firstIndex,lastIndex); 
+    const pokePaginated = characters.slice(firstIndex, lastIndex);
+
+
+    let initialPage = page < 5 ? 1 : (page - 4)
+    let finalPage = lastPage
+    if (page < (lastPage - 5)) {
+        if (page > 5) {
+            finalPage = (page + 4)
+        } else {
+            finalPage = 9
+        }
+    } else {
+    }
+
+
     const numbers = [];
-    for( let i=1; i<=lastPage; i++){
+    for (let i = initialPage; i <= finalPage; i++) {
         numbers.push(i)
     }
 
 
 
+
     return (
         <div className='App'>
-            <div className='container'> 
+            <div className='container'>
                 <h1 className='title-pokedex'>Pokedex</h1>
                 <p className='welcome'>Welcome <b>{user} </b>!!! let's find your favorite pokemon</p>
 
-                <form className='form'   onSubmit={search}>
-                    <input 
-                        placeholder='Search Here...'
+                <form className='form' onSubmit={search}>
+                    <input
+                        placeholder='Search by name...'
                         className='search'
-                        type="text" 
+                        type="text"
                         value={characterSearch}
-                        onChange={e => setcharacterSearch(e.target.value) }/> 
+                        onChange={e => setcharacterSearch(e.target.value)} />
                     <button className="button-search">
                         <i className="fa-solid fa-paper-plane"></i>
                     </button>
@@ -69,46 +84,44 @@ const Pokedex = () => {
                     <option value="">Select a type</option>
                     {
                         types.map(type => (
-                            <option 
+                            <option
                                 value={type.url}
                                 key={type.url}>
                                 {type.name}
-                             </option>
+                            </option>
                         ))
-                        }
+                    }
                 </select>
 
                 <ul className='card-container'>
-                    
-                        { pokePaginated.map(character =>(
-                                <CharacterItem 
-                                characterUrl={character.url ? character.url : character.pokemon.url } 
-                                key={character.url ? character.url : character.pokemon.url} />
-                        ))
-                        
-                        }
-                    
+
+                    {pokePaginated.map(character => (
+                        <CharacterItem
+                            characterUrl={character.url ? character.url : character.pokemon.url}
+                            key={character.url ? character.url : character.pokemon.url} />
+                    ))
+
+                    }
+
                 </ul>
                 <div className='pagination'>
-                    <button 
-                        onClick={()=>setPage (page - 1)}
-                        disabled = {page === 1}
-                        > 
-                        <i className="fas fa-chevron-left"></i>
-                    </button>
+                    <button className='pag'
+                        onClick={() => setPage(page - 1)}
+                        disabled={page === 1}
+                    >&lt;</button>
                     {numbers.map(number => (
-                        <button onClick={()=>setPage(number)}>
+                        <button className={number === page ? 'selected' : 'pag'}
+                            onClick={() => setPage(number)}>
                             {number}
                         </button>
                     ))}
-                    <button 
-                        onClick={()=>setPage (page + 1)}
-                        disabled = {page === lastPage}
-                        > <i className="fas fa-chevron-right"></i>
-                        </button>
+                    <button className='pag'
+                        onClick={() => setPage(page + 1)}
+                        disabled={page === lastPage}
+                    >&gt;</button>
                 </div>
-                
-            </div>   
+
+            </div>
         </div>
     );
 };
